fc49b532afe58a08de583cfdad31ad0477eda99f
raghava <araghava92@gmail.com>
2018-01-30 09:57:22 -0500

    Django Crash Course videos 4 & 5 Changes

--- django/project1/project1/views.py
+++ django/project1/project1/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Welcome to Comp 805!")
+    """
+    Renders home page
+    """
+    greeting = "uStudy - the best study site in the world"
+    today = "tuesday"
+    
+    context = {"our_greeting": greeting, "day_of_week": today}
+    return render(request, "home.html", context=context)

1d7c446d2e10f416d9ccc5d5e60d0f34e369f651
raghava <araghava92@gmail.com>
2018-01-30 11:58:14 -0500

    code formatting for redability

--- django/project1/project1/views.py
+++ django/project1/project1/views.py
@@ -6,8 +6,15 @@ def home(request):
     Renders home page
     """
     greeting = "uStudy - the best study site in the world"
-    days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
-                        "Sunday"]
+    days_of_the_week = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
 
     context = {"our_greeting": greeting, "weekday_list": days_of_the_week}
     return render(request, "home.html", context=context)

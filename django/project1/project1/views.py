b46bdb556fa1bc1760634be61a37115c2d134253
raghava <araghava92@gmail.com>
2018-01-30 10:20:23 -0500

    Django Crash Course videos 6 & 7 Changes

--- django/project1/project1/views.py
+++ django/project1/project1/views.py
@@ -6,7 +6,8 @@ def home(request):
     Renders home page
     """
     greeting = "uStudy - the best study site in the world"
-    today = "tuesday"
-    
-    context = {"our_greeting": greeting, "day_of_week": today}
+    days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+                        "Sunday"]
+
+    context = {"our_greeting": greeting, "weekday_list": days_of_the_week}
     return render(request, "home.html", context=context)

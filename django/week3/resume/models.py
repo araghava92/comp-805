fa337e4bd6c3eb473fd65ffac72ecbb1ed084545
raghava <araghava92@gmail.com>
2018-02-11 20:13:12 -0500

    adds Education model. updates max length in experience model

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Experience(models.Model):
-    title = models.CharField(max_length=None, null=False, blank=False)
-    location = models.CharField(max_length=None, null=False, blank=False)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    location = models.CharField(max_length=100, null=False, blank=False)
     start_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
     end_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+
+
+class Education(models.Model):
+    institution_name = models.CharField(max_length=255, null=False, blank=False)
+    location = models.CharField(max_length=100, null=False, blank=False)
+    degree = models.CharField(max_length=100, null=False, blank=False)
+    major = models.CharField(max_length=100, blank=False)
+    gpa = models.FloatField(blank=False)

0689611681f79bf8556635168c6d519877351a19
raghava <araghava92@gmail.com>
2018-02-11 19:29:50 -0500

    enables url routing to resume app from portfolio project

--- django/week3/portfolio/urls.py
+++ django/week3/portfolio/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("resume/", views.resume, name="resume"),
+    path("resume/", include("resume.urls", namespace="resume")),
     path("portfolio/", views.portfolio, name="portfolio"),
     path("contact/", views.contact, name="contact"),
     path('admin/', admin.site.urls),
--- django/week3/resume/urls.py
+++ django/week3/resume/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "resume"
+
 urlpatterns = [
     path("", views.resume, name="resume"),
 ]

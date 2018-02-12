d69ab7c25e8c3edacd8e39e5b5fd0f6925746210
raghava <araghava92@gmail.com>
2018-02-11 20:11:10 -0500

    adds Experience model

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Experience(models.Model):
+    title = models.CharField(max_length=None, null=False, blank=False)
+    location = models.CharField(max_length=None, null=False, blank=False)
+    start_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)

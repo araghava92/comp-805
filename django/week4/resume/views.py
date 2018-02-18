5981ac32280037777b195567fca22f46636a9a1f
raghava <araghava92@gmail.com>
2018-02-18 15:47:22 -0500

    updates view and resume.html

--- django/week4/resume/views.py
+++ django/week4/resume/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Education as edu
-from .models import Experience as exp
+from .models import Resume
 
 
 def resume(request):
     """
     renders the resume page from resume app
     """
-    exp_set = exp.objects.all().order_by("-end_date")
-    edu_set = edu.objects.all()
+    resume = Resume.objects.first()
 
     return render(request, "resume/resume.html",
-        context={"exp_set": exp_set, "edu_set": edu_set})
+        context={"resume": resume})

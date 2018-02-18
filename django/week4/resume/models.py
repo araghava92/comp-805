92ca57ae36213c00ccd48304018a73cf981fcfe1
raghava <araghava92@gmail.com>
2018-02-18 14:08:10 -0500

    defines Resume model. Adds foreignkeys and makes migration

--- django/week4/resume/models.py
+++ django/week4/resume/models.py
@@ -1,7 +1,40 @@
 from django.db import models
 
+# Resume model
+class Resume(models.Model):
+    # Table fields
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+
+    def get_full_name(self):
+        """
+        returns full name of the person in the form "Firstname Lastname"
+        """
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def get_last_name_first_name(self):
+        """
+        returns name in the form "Lastname, Firstname"
+        """
+        return "{}, {}".format(self.last_name, self.first_name)
+
+    def get_education(self):
+        """
+        returns education for the person defined in the education table
+        """
+        return self.education_set.all()
+
+    def get_experience(self):
+        """
+        returns experience for the person defined in the experience table
+        """
+        return self.experience_set.all()
+
 # Experience model
 class Experience(models.Model):
+    # ForeignKey relationship field
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, default=1)
+
     title = models.CharField(max_length=255, null=False, blank=False)
     location = models.CharField(max_length=100, null=False, blank=False)
     start_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
@@ -14,6 +47,9 @@ class Experience(models.Model):
 
 # Education model
 class Education(models.Model):
+    # ForeignKey relationship field
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, default=1)
+
     institution_name = models.CharField(max_length=255, null=False, blank=False)
     location = models.CharField(max_length=100, null=False, blank=False)
     degree = models.CharField(max_length=100, null=False, blank=False)
